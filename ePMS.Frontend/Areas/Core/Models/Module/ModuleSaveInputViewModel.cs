dfc96716bd358d407aed0b1d407e62e80a24edba
Kamranfarooq3296 <kamranamazon3296@gmail.com>
2026-03-31 14:18:11 +0500

    module backend implementation

---
+++ ePMS.Frontend/Areas/Core/Models/Module/ModuleSaveInputViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ePMS.Frontend.Areas.Core.Models.Module
+{
+	public class ModuleSaveInputViewModel
+	{
+        public long Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string Details { get; set; }
+
+        public string ModuleUrl { get; set; }
+
+        public string IconClass { get; set; }
+
+        public int Priority { get; set; }
+
+        public long CompanyId { get; set; }
+
+        public long BranchId { get; set; }
+
+        public long  CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public long LastModifiedBy { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+        public long DeletedBy { get; set; }
+
+        public DateTime DeletedDate { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+    }
+}
---
+++ ePMS.Frontend/Areas/Core/Models/Module/ModuleTableOutputViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ePMS.Frontend.Areas.Core.Models.Module
+{
+	public class ModuleTableOutputViewModel
+	{
+        public long ID { get; set; }                     
+        public string Description { get; set; }           
+        public string? Detail { get; set; }               
+        public string? ModuleUrl { get; set; }            // NULL
+        public string? IconClass { get; set; }            // NULL
+        public int Priority { get; set; }                 // NOT NULL
+        public long CompanyID { get; set; }               // NOT NULL
+        public long? BranchID { get; set; }               // NULL
+        public long CreatedBy { get; set; }               
+        public DateTime CreatedDate { get; set; }         
+        public long? LastModifiedBy { get; set; }         
+        public DateTime? LastModifiedDate { get; set; }   
+        public long? DeletedBy { get; set; }              
+        public DateTime? DeletedDate { get; set; }        
+        public bool IsDeleted { get; set; }
+    }
+}
