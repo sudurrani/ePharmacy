8a524b139eb08c1afa2adc478891136d34115d64
KamranFarooq8BHA <kamran.farooq.bha@outlook.com>
2026-04-06 14:58:52 +0500

    PH-31 RoleRight Backend Implementation Without List Implementation.... Pushing to pulll latest changes just

---
+++ ePMS.Frontend/Areas/UserManagement/Controllers/RoleRightController.cs
@@ -0,0 +1,50 @@
+﻿using ePMS.Frontend.Areas.UserManagement.Models.RoleRight;
+using ePMS.Frontend.CommonClasses;
+using ePMS.Frontend.Models.ViewModels.OutputViewModel.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ePMS.Frontend.Areas.UserManagement.Controllers
+{
+    public class RoleRightController : Controller
+    {
+        private Repository _respository = new Repository("ePharmacyConnection");
+        SqlDynamicParameters sqlDynamicParameters;
+        ResponseOutputDto _responseOutputDto = new ResponseOutputDto();
+
+        [HttpGet]
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Save(RoleRightSaveInputViewModel roleRightSaveInputViewModel)
+        {
+            return View();
+        }
+
+        public ActionResult GetAll()
+        {
+            return View();
+        }
+
+        public ActionResult GetByID()
+        {
+            return View();
+        }
+
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+
+
+
+        
+    } 
+}
---
+++ ePMS.Frontend/Areas/UserManagement/Models/RoleRight/RoleRightTableOutputViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ePMS.Frontend.Areas.UserManagement.Models.RoleRight
+{
+    public class RoleRightTableOutputViewModel
+    {
+    }
+}
