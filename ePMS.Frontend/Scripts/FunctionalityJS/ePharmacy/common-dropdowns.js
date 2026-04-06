2670be976eeff009141061b42c6ad091fb575858
Shahid Ullah <shahid@bha-accountancy.com>
2026-04-06 14:46:41 +0500

    Added loadRoleDropdownList function

--- ePMS.Frontend/Scripts/FunctionalityJS/ePharmacy/common-dropdowns.js
+++ ePMS.Frontend/Scripts/FunctionalityJS/ePharmacy/common-dropdowns.js
@@ -6,3 +6,12 @@
         }
     });
 }
+function loadRoleDropdownList(inputID = null, selectedValue = 0, selectedText = null, defaultText = null, addNewOption = null) {
+    ajaxRequest({
+        url: '/Dropdowns/Get', type: 'POST', data: {
+            Table: '[Setup.Role]', Columns: 'ID Value, Description Text', Condition: 'WHERE IsDeleted =0'
+        }, callBack: function (responseJSON) {
+            bindJQueryDropdownList(responseJSON.resultJSON, $('#' + (inputID == null ? 'RoleID' : inputID)), (defaultText == null ? 'Select Role' : defaultText), selectedValue, selectedText, addNewOption);
+        }
+    });
+}
