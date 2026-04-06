4834e6634c86fd24bb6f5c32571eb6fd8378ba1b
Shahid Ullah <shahid@bha-accountancy.com>
2026-04-06 17:12:19 +0500

    Added loadParentDropdownList

--- ePMS.Frontend/Scripts/FunctionalityJS/ePharmacy/common-dropdowns.js
+++ ePMS.Frontend/Scripts/FunctionalityJS/ePharmacy/common-dropdowns.js
@@ -6,6 +6,15 @@
         }
     });
 }
+function loadParentDropdownList(inputID = null, selectedValue = 0, selectedText = null, defaultText = null, addNewOption = null) {
+    ajaxRequest({
+        url: '/Dropdowns/Get', type: 'POST', data: {
+            Table: 'ePharmacy.dbo.SubModule', Columns: 'ID Value, Description Text', Condition: 'WHERE IsDeleted =0'
+        }, callBack: function (responseJSON) {
+            bindJQueryDropdownList(responseJSON.resultJSON, $('#' + (inputID == null ? 'ParentID' : inputID)), (defaultText == null ? 'Select Parent' : defaultText), selectedValue, selectedText, addNewOption);
+        }
+    });
+}
 function loadRoleDropdownList(inputID = null, selectedValue = 0, selectedText = null, defaultText = null, addNewOption = null) {
     ajaxRequest({
         url: '/Dropdowns/Get', type: 'POST', data: {
